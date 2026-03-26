0231e2b1121f40217761d298fa4d3a1e12dca564
bas080 <bassimhuis@gmail.com>
2026-03-26 02:30:13 +0200

    Format code according to stylua

--- check.lua
+++ check.lua
@@ -2,44 +2,44 @@ local property, gen = luanti_check()
 local range = 40
 
 property("Spawns vines", function(t)
-	local player = gen.player_pos({
-		pos = { y = range * 2 },
-	})
+    local player = gen.player_pos({
+        pos = { y = range * 2 },
+    })
 
-	t.on_emerge(function()
-		t.done(player)
-	end, range)
+    t.on_emerge(function()
+        t.done(player)
+    end, range)
 end, function(t, player)
-	local _, node_pos = gen.node_pos({ player = player, radius = range, nodenames = "group:vines" })
+    local _, node_pos = gen.node_pos({ player = player, radius = range, nodenames = "group:vines" })
 
-	gen.player_pos({
-		player = player,
-		pos = node_pos,
-	})
+    gen.player_pos({
+        player = player,
+        pos = node_pos,
+    })
 
-	t.done(player, node_pos)
+    t.done(player, node_pos)
 end, function(t, player, node_pos)
-	-- find all nodes in range and check that they follow certain rules by iterating over them.
-	-- vine nodes should always  b
-	-- must always have either a node above or below.
+    -- find all nodes in range and check that they follow certain rules by iterating over them.
+    -- vine nodes should always  b
+    -- must always have either a node above or below.
 
-	local found = core.find_nodes_in_area(vector.add(node_pos, -range), vector.add(node_pos, range), { "group:vines" })
+    local found = core.find_nodes_in_area(vector.add(node_pos, -range), vector.add(node_pos, range), { "group:vines" })
 
-	gen.where(#found > 0)
+    gen.where(#found > 0)
 
-	for _, pos in ipairs(found) do
-		local down = vector.add(pos, { y = -1, x = 0, z = 0 })
-		local up = vector.add(pos, { y = 1, x = 0, z = 0 })
+    for _, pos in ipairs(found) do
+        local down = vector.add(pos, { y = -1, x = 0, z = 0 })
+        local up = vector.add(pos, { y = 1, x = 0, z = 0 })
 
-		if core.get_node(down).name == "air" and core.get_node(up).name == "air" then
-			player:set_pos(pos)
+        if core.get_node(down).name == "air" and core.get_node(up).name == "air" then
+            player:set_pos(pos)
 
-			-- t.ok(core.get_node(down).name ~= "air" or core.get_node(up).name ~= "air",
-			error("Should not have dangling vines. This check is not good enough though.")
-		end
+            -- t.ok(core.get_node(down).name ~= "air" or core.get_node(up).name ~= "air",
+            error("Should not have dangling vines. This check is not good enough though.")
+        end
 
-		-- "Must have a node either above or below the vine")
-	end
+        -- "Must have a node either above or below the vine")
+    end
 
-	t.done()
+    t.done()
 end)
